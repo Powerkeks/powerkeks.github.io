8dd3ae5a4238758247856c2774cfabb07916f70f
Powerkeks <82614028+Powerkeks@users.noreply.github.com>
2021-07-19 21:01:27 +0200

    0.7

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-getUser("powerkeks");
+getUser("Powerkeks");
 
 async function getUser(username) {
     const resp = await fetch(APIURL + username);
